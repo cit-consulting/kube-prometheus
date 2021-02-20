e04e2450ad2890668756afb52b94b546031e53d3
Alexander Ivanov <aivanov@citc.ru>
2021-02-20 13:14:12 +0300

    manifests generating citc scripts

---
+++ build-citc.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# This script uses arg $1 (name of *.jsonnet file to use) to generate the manifests/*.yaml files.
+
+set -e
+set -x
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+
+# Make sure to use project tooling
+PATH="$(pwd)/tmp/bin:${PATH}"
+
+# Make sure to start with a clean 'manifests' dir
+rm -rf manifests
+mkdir -p manifests/setup
+mkdir -p manifests/node-exporter
+mkdir -p manifests/blackbox-exporter
+mkdir -p manifests/kube-state-metrics
+mkdir -p manifests/alertmanager
+mkdir -p manifests/prometheus
+mkdir -p manifests/prometheus-adapter
+mkdir -p manifests/grafana
+mkdir -p manifests/kubernetes
+
+# Calling gojsontoyaml is optional, but we would like to generate yaml, not json
+jsonnet -J vendor -m manifests "${1-example.jsonnet}" | xargs -I{} sh -c 'cat {} | gojsontoyaml > {}.yaml' -- {}
+
+# Make sure to remove json files
+find manifests -type f ! -name '*.yaml' -delete
+rm -f kustomization
+
